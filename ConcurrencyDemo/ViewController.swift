c6447a23e2cc15bfd0ce6d9c38297992f7612d50
Cntt04 <Admin04@Cntt04s-iMac.local>
2017-05-10 14:15:47 +0700

    Commit

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -32,6 +32,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sliderValueLabel: UILabel!
     
+    var queue = OperationQueue()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -43,23 +46,66 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didClickOnStart(_ sender: AnyObject) {
+        queue = OperationQueue()
         
-        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
-        self.imageView1.image = img1
+        let operation1 = BlockOperation(block: {
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            OperationQueue.main.addOperation({
+                self.imageView1.image = img1
+            })
+        })
         
-        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
-        self.imageView2.image = img2
+        operation1.completionBlock = {
+            print("Operation 1 completed, cancelled:\(operation1.isCancelled)")
+        }
+        queue.addOperation(operation1)
         
-        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
-        self.imageView3.image = img3
+        let operation2 = BlockOperation(block: {
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            OperationQueue.main.addOperation({
+                self.imageView2.image = img2
+            })
+        })
+        operation2.addDependency(operation1)
+        operation2.completionBlock = {
+            print("Operation 2 completed, cancelled:\(operation2.isCancelled)")
+        }
+        queue.addOperation(operation2)
         
-        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
-        self.imageView4.image = img4
+        
+        let operation3 = BlockOperation(block: {
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            OperationQueue.main.addOperation({
+                self.imageView3.image = img3
+            })
+        })
+        operation3.addDependency(operation2)
+        
+        operation3.completionBlock = {
+            print("Operation 3 completed, cancelled:\(operation3.isCancelled)")
+        }
+        queue.addOperation(operation3)
+        
+        let operation4 = BlockOperation(block: {
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            OperationQueue.main.addOperation({
+                self.imageView4.image = img4
+            })
+        })
+        
+        operation4.completionBlock = {
+            print("Operation 4 completed, cancelled:\(operation4.isCancelled)")
+        }
+        queue.addOperation(operation4)
+
         
     }
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         
         self.sliderValueLabel.text = "\(sender.value * 100.0)"
+    }
+    @IBAction func didClickOnCancel(_ sender: Any) {
+        self.queue.cancelAllOperations()
     }
 
 }
